7f8323525af61ae5cf8365031d8a2a81d23433e7
ysprod <yayasidibeproduction@gmail.com>
2026-01-22 01:06:38 +0000

    fix: add ConsultationModel import to RubriqueModule for DI error

--- src/rubriques/rubrique.module.ts
+++ src/rubriques/rubrique.module.ts
@@ -5,11 +5,14 @@ import { RubriqueService } from './rubrique.service';
 import { RubriqueController } from './rubrique.controller';
 import { UserConsultationChoice, UserConsultationChoiceSchema } from '../consultations/schemas/user-consultation-choice.schema';
 
+import { Consultation, ConsultationSchema } from '../consultations/schemas/consultation.schema';
+
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Rubrique.name, schema: RubriqueSchema },
       { name: UserConsultationChoice.name, schema: UserConsultationChoiceSchema },
+      { name: Consultation.name, schema: ConsultationSchema },
     ]),
   ],
   providers: [RubriqueService],
